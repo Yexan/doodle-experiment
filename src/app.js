4b4ca5f80ee77fc70dc005ef8b8267f16b4419b9
adrien <adrien.martinet@rosemood.fr>
2021-11-17 12:03:55 +0100

    Add first Doodle exemple refresh every 2 seconds

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
-console.log('Hello world!');
+import 'css-doodle';
+
+const doodle = document.querySelector('.doodle');
+
+const doodleRefreshInterval = setInterval(
+  () => doodle.update(),
+  2000
+);
